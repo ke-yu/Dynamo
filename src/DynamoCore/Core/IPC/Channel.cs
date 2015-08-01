d96ad3b84ccb75ae07124515c879fa0060f5be17
Ben Goh <benglin@outlook.com>
2015-08-01 12:25:14 +0800

    Added more contents to Channel

--- src/DynamoCore/Core/IPC/Channel.cs
+++ src/DynamoCore/Core/IPC/Channel.cs
@@ -1,11 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace Dynamo.Core.IPC
 {
-    public abstract class Channel
+    public class Channel
     {
+        private Process process;
+
+        internal string Name { get; private set; }
+
+        internal Channel(Process process, string name)
+        {
+            this.process = process;
+            this.Name = name;
+        }
+
+        internal void Close()
+        {
+            // TODO: Close out the communication channel and 
+            // shutdown the corresponding execution instance.
+        }
     }
 }
